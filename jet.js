bf3d389ad58081af2a1a13273320952f031e5a52
Yishaik <yishai.k@gmail.com>
2017-03-13 10:03:32 +0200

    added takeoff

--- jet.js
+++ jet.js
@@ -5,6 +5,9 @@ var Jet = function (color, size) {
   this.fly = function () {
     console.log('whoosh!');
   }
+  this.takeOff = function () {
+    console.log('lift off!');
+}
 }
 
 module.exports = Jet;
